8465af13e4f5946e91bedf8b2cdefd902f92b077
LH <littleherry@ultracombos.com>
2017-03-29 16:43:16 +0800

    Modified batch to copy SpoutPanel from SpoutSDK.

--- 000_copy_files_from_SpoutSDK.bat
+++ 000_copy_files_from_SpoutSDK.bat
@@ -7,9 +7,15 @@
 
 @PUSHD %~d0%~p0
 
-@SET filecpy=XCOPY /Y /D /S
+@SET foldercpy=XCOPY /Y /D /S
+@SET filecpy=XCOPY /Y
+
 @SET INC_DIR=libs\SpoutSDK\include\
 @SET LIB_DIR=libs\SpoutSDK\lib\
+@SET BIN_DIR=bin\
+@SET SpoutPanel=SpoutPanel.exe 
+
+@SET SPOUT_ROOT_DIR=%SPOUT_SDK_DIR%..\
 
 @IF EXIST "%INC_DIR%" (
 	@RD /S /Q %INC_DIR%
@@ -19,10 +25,12 @@
 	@RD /S /Q %LIB_DIR%
 )
 
-@%filecpy% %SPOUT_SDK_DIR%Binaries\*.lib %LIB_DIR%
-@%filecpy% %SPOUT_SDK_DIR%Binaries\*.dll %LIB_DIR%
+@%foldercpy% %SPOUT_SDK_DIR%Binaries\*.lib %LIB_DIR%
+@%foldercpy% %SPOUT_SDK_DIR%Binaries\*.dll %LIB_DIR%
 
-@%filecpy% %SPOUT_SDK_DIR%Include %INC_DIR%
+@%foldercpy% %SPOUT_SDK_DIR%Include %INC_DIR%
+
+@%filecpy% %SPOUT_ROOT_DIR%SPOUTPANEL\SpoutPanel.exe %BIN_DIR%
 
 @PAUSE
 @POPD
